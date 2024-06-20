1cd8632493ee86fd40881fd5a1292a0a885dff07
Fakhrus Syakir <161460143+KHR00S@users.noreply.github.com>
2024-06-20 18:04:40 +0700

    update tampilan home

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -17,13 +17,23 @@ def plot_rainfall_line(df):
     return fig
 
 def app():
-    # Judul dan Informasi mengenai Dasboard
-    st.title("Dashboard Pemantauan dan Prediksi Upwelling Berbasis Indikator Iklim di Danau Laut Tawar")
-    st.write("Selamat datang di Dashboard Pemantauan dan Prediksi Upwelling berbasis indikator iklim Danau Laut Tawar! Masyarakat setempat memanfaatkan danau ini sebagai salah satu sumber mata pencaharian utama. Dengan manggabungkan data Dalam setahun, potensi produksi ikan di Danau Laut Tawar dapat mencapai 196 ton. Angka ini cukup fantastis dan menunjukkan betapa pentingnya peran Danau Laut Tawar dalam menyokong ekonomi lokal serta menyediakan pangan bagi masyarakat setempat. Namun, perubahan iklim yang tidak menentu mengganggu kestabilan produksi ikan di Danau Laut Tawar. Salah satu faktor penyebabnya adalah fenomena upwelling. Pada tahun 2017, fenomena ini pernah merugikan pembudidaya Keramba Jaring Apung (KJA) di Danau Laut Tawar, higga mengakibatkan kerugian mencapai ratusan juta rupiah."
-            )
+    # Language Selection
+    language = st.sidebar.selectbox("Select Language / Pilih Bahasa", ["English", "Indonesian"])
+
+    if language == "English":
+        st.title("Monitoring and Prediction Dashboard for Climate Indicator-Based Upwelling in Lake Tawar")
+        st.write("Welcome to the Monitoring and Prediction Dashboard for climate indicator-based upwelling in Lake Tawar! The local community relies on this lake as a primary livelihood source.")
+        st.write("Within a year, potential fish production in Lake Tawar can reach 196 tons. This figure is quite significant and underscores the lake's importance in supporting the local economy and providing food for the community.")
+        st.write("However, unpredictable climate changes disrupt fish production stability in Lake Tawar. One contributing factor is the phenomenon of upwelling. In 2017, this phenomenon adversely affected floating cage aquaculture (KJA) in Lake Tawar, resulting in losses amounting to hundreds of millions of rupiah.")
+
+    elif language == "Indonesian":
+        st.title("Dashboard Pemantauan dan Prediksi Upwelling Berbasis Indikator Iklim di Danau Laut Tawar")
+        st.write("Selamat datang di Dashboard Pemantauan dan Prediksi Upwelling berbasis indikator iklim Danau Laut Tawar! Masyarakat setempat memanfaatkan danau ini sebagai salah satu sumber mata pencaharian utama.")
+        st.write("Dalam setahun, potensi produksi ikan di Danau Laut Tawar dapat mencapai 196 ton. Angka ini cukup fantastis dan menunjukkan betapa pentingnya peran Danau Laut Tawar dalam menyokong ekonomi lokal serta menyediakan pangan bagi masyarakat setempat.")
+        st.write("Namun, perubahan iklim yang tidak menentu mengganggu kestabilan produksi ikan di Danau Laut Tawar. Salah satu faktor penyebabnya adalah fenomena upwelling. Pada tahun 2017, fenomena ini pernah merugikan pembudidaya Keramba Jaring Apung (KJA) di Danau Laut Tawar, hingga mengakibatkan kerugian mencapai ratusan juta rupiah.")
 
     # Load Dataset
-    df = load_data("Dashboard/data/HASIL_CLUSTERING.csv")
+    df = load_data("Dashboard\data\HASIL_CLUSTERING.csv")
 
     # Data Historis Banjir
     df_class = df.copy()
@@ -31,37 +41,43 @@ def app():
     df_class.set_index('DATE', inplace=True)
 
     # Pemfilteran Data Berdasarkan Range Waktu
-    date_range = st.date_input("Pilih Rentang Waktu", [df_class.index.min(), df_class.index.max()], key="date_range")
+    date_range = st.date_input("Select Date Range / Pilih Rentang Waktu", [df_class.index.min(), df_class.index.max()], key="date_range")
     start_date, end_date = date_range
     filtered_df_class = df_class.loc[start_date:end_date]
 
     # Menampilkan Data Historis Banjir
-    st.header("Deskripsi kolom dari table tersebut")
+    st.header("Description of the columns from the table")
     st.write(filtered_df_class)
 
     # Menampilkan penjelasan dari struktur data
-    kolomdesc = "1.  DATE\t: Merupakan kolom yang mencatat tanggal indikator iklim\
-             \n2.  ALLSKY_KT\t: Indeks kejernihan insolasi langit\
-             \n3.  T2M\t        : Suhu udara rata-rata pada ketinggian 2 meter (°C)\
-             \n4.  TS\t        : Suhu rata-rata di permukaan bumi (°C)\
-             \n5.  PRECTOTCORR\t: Curah hujan (mm)\
-             \n6.  PS\t        : Rata-rata tekanan permukaan di permukaan bumi (kPA)\
-             \n7.  WS10M\t: Kecepatan angin rata-rata pada ketinggian 10 meter (m/s)\
-             \n8.  Status\t: Potensi Kejadian Upwelling"
+    st.markdown("Description of the columns from the table:")
+    kolomdesc = '\n1.  DATE\t: Records the date climate indicators were recorded\
+                 \n2.  ALLSKY_KT\t: Insolation clarity index of the sky\
+                 \n3.  T2M\t: Average air temperature at a height of 2 meters (°C)\
+                 \n4.  TS\t: Average temperature at the Earth's surface (°C)\
+                 \n5.  PRECTOTCORR\t: Precipitation (mm)\
+                 \n6.  PS\t: Average surface pressure at the Earth's surface (kPA)\
+                 \n7.  WS10M\t: Average wind speed at a height of 10 meters (m/s)\
+                 \n8.  Status\t: Indicates rainfall (mm) at 15:00\
+                 \n9.  hujan_2100\t: Potential Upwelling Event\
+                 \n10. min_hujan\t: Potential Upwelling Event'
     st.text(kolomdesc)
-    
-    
+
     # Menampilkan Plot Kejadian "Banjir" dan "Tidak Banjir" 
     fig = go.Figure()
-    for Status, color in zip(filtered_df_class['Status'].unique(), ['red', 'green']):  # Choose your preferred colors
+    for Status, color in zip(filtered_df_class['Status'].unique(), ['green', 'red']):  # Choose your preferred colors
         filtered_df = filtered_df_class[filtered_df_class['Status'] == Status]
         fig.add_trace(go.Scatter(x=filtered_df.index, y=filtered_df['PRECTOTCORR'],
-                             mode='markers', name=Status, line=dict(color=color)))
-    fig.update_layout(title='Potensi Upwelling vs Tidak Berpotensi Upwelling berdasarkan Indikator Iklim',
+                             mode='lines+markers', name=Status, line=dict(color=color)))
+    fig.update_layout(title='Potential Upwelling vs Non-Upwelling based on Climate Indicators',
                   xaxis_title='Date',
                   yaxis_title='PRECTOTCORR',
                   template='plotly_dark')
     st.plotly_chart(fig, use_container_width=True)
+
+if __name__ == "__main__":
+    app()
+
 '''
     # Data Historis Curah Hujan
     st.header("Data Historis Curah Hujan")
