ec1dd9c96c4d413cb553d14b07f31d06b9543dc1
Fakhrus Syakir <161460143+KHR00S@users.noreply.github.com>
2024-09-02 11:44:54 +0700

    about bahasa

--- Dashboard/about.py
+++ Dashboard/about.py
@@ -7,115 +7,7 @@ def app():
     # Language selection
     lang = st.selectbox("Select Language / Pilih Bahasa", ["English", "Bahasa Indonesia"])
 
-    if lang == "English":
-        # English content
-        st.title("About Us")
-        st.header("Project Description")
-        st.markdown("""
-            This dashboard is part of the output from the Program Kreatifitas Mahasiswa – Riset Eksakta (PKM - RE) conducted by students of Syiah Kuala University. This project is designed to help Floating Net Cage farmers in Danau Laut Tawar make decisions in fish farming.
-        """)
-
-        col_1, col_2, col_3 = st.columns(3)
-        with col_1:
-            st.header("Vision")
-            st.markdown("""
-            To be an innovative solution provider in mitigating upwelling risks and providing accurate information for decision-making by Danau Laut Tawar KJA farmers.
-            """)
-        with col_2:
-            st.header("Mission")
-            st.markdown("""
-            1. Accurately collect and analyze upwelling events and climate indicators.
-            2. Develop reliable upwelling prediction models based on historical data.
-            3. Provide an interactive dashboard to monitor and forecast upwelling risks.
-            4. Effectively disseminate information on upwelling risks to KJA farmers.
-            """)
-        with col_3:
-            st.header("Goals")
-            st.markdown("""
-            1. Provide a reliable source of information related to upwelling risks in Danau Laut Tawar.
-            2. Increase awareness and preparedness of Danau Laut Tawar KJA farmers against potential upwelling risks.
-            3. Support mitigation and countermeasures against upwelling risks in Danau Laut Tawar.
-            """)
-
-        st.header("Datasets Used")
-        st.markdown("""
-            We use several datasets in this project. Here are some of them:
-            1. Upwelling events data in Danau Laut Tawar.
-            2. Danau Laut Tawar climate data - [NASA Prediction Of Worldwide Energy Resources](https://power.larc.nasa.gov/)
-        """)
-
-        st.header("Models Used")
-        st.markdown("""
-            #### Forecasting Models (SVARMA and Seasonal Autoregressive):
-            We use two models, SVARMA and Seasonal Autoregressive, to forecast climate indicators at various time intervals:
-            - All sky insolation clearness index: MSE = 0,0526, RMSE = 0,2295, MAE = 0,1815
-            - Temperature: MSE = 0,0341, RMSE = 0,1847, MAE = 0,1514
-            - Precipitation: MSE = 0,0738, RMSE = 0,2721, MAE = 0,2191
-            - Surface pressure: MSE = 0,0399, RMSE = 0,1998, MAE = 0,1602
-            - Wind speed: MSE = 0,0287, RMSE = 0,1697, MAE = 0,1347
-
-            #### Prediction Model:
-            We use the SVM model to predict upwelling:
-            - SVM model with F1-Score = 0,985
-        """)
-
-        st.header("Technologies / Tools Used")
-        st.markdown("""
-            - **Streamlit:** For building the user interface.
-            - **Pandas & R:** For data manipulation and analysis.
-            - **Joblib:** For saving and loading machine learning models.
-            - **Plotly Express and Plotly Graph Objects:** Libraries for creating interactive data visualizations.
-        """)
-
-        st.header("Social Media")
-        st.markdown("""
-            <a href="https://www.instagram.com/pkmre_upwelling?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" style="width:30px;height:30px;">
-            @pkmre_upwelling
-            </a>
-            """, unsafe_allow_html=True)
-        st.markdown("""
-            <a href="https://www.tiktok.com/@pkmre_upwelling?_t=8n8OhzhoUsr&_r=1">
-            <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" alt="TikTok" style="width:30px;height:30px;background-color:white;">
-            @pkmre_upwelling
-            </a>
-            """, unsafe_allow_html=True)
-
-        st.header("Contact")
-        st.markdown("""
-            If you have any questions or feedback, please contact us:
-        """)
-        col_4, col_5 = st.columns(2)
-        with col_4:
-            st.markdown("""
-                - **Muhammad Zia Ulhaq**
-                    - Email: [ziaswatfbicia@gmail.com](mailto:ziaswatfbicia@gmail.com) 
-                    - LinkedIn: [Muhammad Zia Ulhaq](https://www.linkedin.com/in/muhammad-zia-ulhaq-8373112b9/) 
-
-                - **Zahra Ifma Aziza**
-                    - Email: [zahraifmaa@gmail.com](mailto:zahraifmaa@gmail.com) 
-                    - LinkedIn: [Zahra Ifma Aziza](https://www.linkedin.com/in/zahra-ifma-aziza) 
-
-                - **Muhammad Farid**
-                    - Email: [mhdfaridz93@gmail.com](mailto:mhdfaridz93@gmail.com) 
-                    - LinkedIn: [Muhammad Farid](https://www.linkedin.com/in/MHD-FARID)  
-            """)
-        with col_5:
-            st.markdown("""
-                - **Fakhrus Syakir**
-                    - Email: [fakhroosyakir@gmail.com](mailto:fakhroosyakir@gmail.com) 
-                    - LinkedIn: [Fakhrus Syakir](https://www.linkedin.com/in/fakhrus-syakir-65bb72205/) 
-
-                - **Teuku Muhammad Faiz Nuzullah**
-                    - Email: [faiznuzullah@gmail.com](mailto:faiznuzullah@gmail.com) 
-                    - LinkedIn: [Teuku Muhammad Faiz Nuzullah](https://www.linkedin.com/in/teuku-muhammad-f-4a906a239) 
-
-                - **Novi Reandy Sasmita, S.Si., M.Sc.**
-                    - Email: [novireandys@usk.ac.id](mailto:novireandys@usk.ac.id) 
-                    - LinkedIn: [Novi Reandy Sasmita, S.Si., M.Sc.](https://www.linkedin.com/in/novi-reandy-sasmita-93371a72/) 
-            """)
-
-    else:
+    if lang == "Bahasa Indonesia":
         # Indonesian content
         st.title("Tentang Kami")
         st.header("Deskripsi Projek")
@@ -226,3 +118,112 @@ def app():
                     - LinkedIn: [Novi Reandy Sasmita, S.Si., M.Sc.](https://www.linkedin.com/in/novi-reandy-sasmita-93371a72/) 
             """)
 
+
+
+    else:
+        # English content
+        st.title("About Us")
+        st.header("Project Description")
+        st.markdown("""
+            This dashboard is part of the output from the Program Kreatifitas Mahasiswa – Riset Eksakta (PKM - RE) conducted by students of Syiah Kuala University. This project is designed to help Floating Net Cage farmers in Danau Laut Tawar make decisions in fish farming.
+        """)
+
+        col_1, col_2, col_3 = st.columns(3)
+        with col_1:
+            st.header("Vision")
+            st.markdown("""
+            To be an innovative solution provider in mitigating upwelling risks and providing accurate information for decision-making by Danau Laut Tawar KJA farmers.
+            """)
+        with col_2:
+            st.header("Mission")
+            st.markdown("""
+            1. Accurately collect and analyze upwelling events and climate indicators.
+            2. Develop reliable upwelling prediction models based on historical data.
+            3. Provide an interactive dashboard to monitor and forecast upwelling risks.
+            4. Effectively disseminate information on upwelling risks to KJA farmers.
+            """)
+        with col_3:
+            st.header("Goals")
+            st.markdown("""
+            1. Provide a reliable source of information related to upwelling risks in Danau Laut Tawar.
+            2. Increase awareness and preparedness of Danau Laut Tawar KJA farmers against potential upwelling risks.
+            3. Support mitigation and countermeasures against upwelling risks in Danau Laut Tawar.
+            """)
+
+        st.header("Datasets Used")
+        st.markdown("""
+            We use several datasets in this project. Here are some of them:
+            1. Upwelling events data in Danau Laut Tawar.
+            2. Danau Laut Tawar climate data - [NASA Prediction Of Worldwide Energy Resources](https://power.larc.nasa.gov/)
+        """)
+
+        st.header("Models Used")
+        st.markdown("""
+            #### Forecasting Models (SVARMA and Seasonal Autoregressive):
+            We use two models, SVARMA and Seasonal Autoregressive, to forecast climate indicators at various time intervals:
+            - All sky insolation clearness index: MSE = 0,0526, RMSE = 0,2295, MAE = 0,1815
+            - Temperature: MSE = 0,0341, RMSE = 0,1847, MAE = 0,1514
+            - Precipitation: MSE = 0,0738, RMSE = 0,2721, MAE = 0,2191
+            - Surface pressure: MSE = 0,0399, RMSE = 0,1998, MAE = 0,1602
+            - Wind speed: MSE = 0,0287, RMSE = 0,1697, MAE = 0,1347
+
+            #### Prediction Model:
+            We use the SVM model to predict upwelling:
+            - SVM model with F1-Score = 0,985
+        """)
+
+        st.header("Technologies / Tools Used")
+        st.markdown("""
+            - **Streamlit:** For building the user interface.
+            - **Pandas & R:** For data manipulation and analysis.
+            - **Joblib:** For saving and loading machine learning models.
+            - **Plotly Express and Plotly Graph Objects:** Libraries for creating interactive data visualizations.
+        """)
+
+        st.header("Social Media")
+        st.markdown("""
+            <a href="https://www.instagram.com/pkmre_upwelling?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" style="width:30px;height:30px;">
+            @pkmre_upwelling
+            </a>
+            """, unsafe_allow_html=True)
+        st.markdown("""
+            <a href="https://www.tiktok.com/@pkmre_upwelling?_t=8n8OhzhoUsr&_r=1">
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" alt="TikTok" style="width:30px;height:30px;background-color:white;">
+            @pkmre_upwelling
+            </a>
+            """, unsafe_allow_html=True)
+
+        st.header("Contact")
+        st.markdown("""
+            If you have any questions or feedback, please contact us:
+        """)
+        col_4, col_5 = st.columns(2)
+        with col_4:
+            st.markdown("""
+                - **Muhammad Zia Ulhaq**
+                    - Email: [ziaswatfbicia@gmail.com](mailto:ziaswatfbicia@gmail.com) 
+                    - LinkedIn: [Muhammad Zia Ulhaq](https://www.linkedin.com/in/muhammad-zia-ulhaq-8373112b9/) 
+
+                - **Zahra Ifma Aziza**
+                    - Email: [zahraifmaa@gmail.com](mailto:zahraifmaa@gmail.com) 
+                    - LinkedIn: [Zahra Ifma Aziza](https://www.linkedin.com/in/zahra-ifma-aziza) 
+
+                - **Muhammad Farid**
+                    - Email: [mhdfaridz93@gmail.com](mailto:mhdfaridz93@gmail.com) 
+                    - LinkedIn: [Muhammad Farid](https://www.linkedin.com/in/MHD-FARID)  
+            """)
+        with col_5:
+            st.markdown("""
+                - **Fakhrus Syakir**
+                    - Email: [fakhroosyakir@gmail.com](mailto:fakhroosyakir@gmail.com) 
+                    - LinkedIn: [Fakhrus Syakir](https://www.linkedin.com/in/fakhrus-syakir-65bb72205/) 
+
+                - **Teuku Muhammad Faiz Nuzullah**
+                    - Email: [faiznuzullah@gmail.com](mailto:faiznuzullah@gmail.com) 
+                    - LinkedIn: [Teuku Muhammad Faiz Nuzullah](https://www.linkedin.com/in/teuku-muhammad-f-4a906a239) 
+
+                - **Novi Reandy Sasmita, S.Si., M.Sc.**
+                    - Email: [novireandys@usk.ac.id](mailto:novireandys@usk.ac.id) 
+                    - LinkedIn: [Novi Reandy Sasmita, S.Si., M.Sc.](https://www.linkedin.com/in/novi-reandy-sasmita-93371a72/) 
+            """)
