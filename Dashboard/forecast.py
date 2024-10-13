29c2c52bde9ef91ef4aa0dbdb06f2ceb005a1923
Fakhrus Syakir <fakhrussyakir@212.11.123.172.rev.iijmobile.jp>
2024-10-13 21:41:46 +0700

    forecast judul

--- Dashboard/forecast.py
+++ Dashboard/forecast.py
@@ -7,7 +7,7 @@ def app():
     st.title("Data Display")
 
     # Load the CSV file
-    csv_path = 'Dashboard/data/Data_Hasil_Forcast_2_Tahun_(2024-2025).csv'
+    csv_path = 'Dashboard/data/Data_Hasil_Forcast_2_Tahun_(2024-2025) copy.csv'
     df = pd.read_csv(csv_path)
 
     # Convert the DATE column to datetime format
